41046b73121a4b068737524507f7664a23b44459
machao123045 <machao123045@jd.com>
2021-01-12 10:18:47 +0800

    盛最多水的容器

--- leetcode/src/main/java/盛最多水的容器.java
+++ leetcode/src/main/java/盛最多水的容器.java
@@ -7,18 +7,21 @@
 public class 盛最多水的容器 {
     public int maxArea(int[] height) {
 
+        int max = 0;
         int l = 0;
         int r = height.length-1;
-        int ars = 0;
-        while(l<r){
-            int area = Math.min(height[l],height[r])*(r-1);
-            ars = Math.max(ars,area);
-            if(height[l]<=height[r]){
-                ++l;
-            }else {
-                --r;
+
+        while (l<r){
+            max = Math.max(max,Math.min(height[l],height[r]) * (r-l));
+
+            if (height[l]<height[r]){
+                l++;
+            }else{
+                r--;
             }
         }
-        return ars;
+        return max;
+
     }
+
 }
